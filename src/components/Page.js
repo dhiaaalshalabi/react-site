1f22f30a81af357bd06d42cc610320906eadf464
Dhiaa Al-Shalabi <dhiaa.alshalabi@gmail.com>
2022-10-07 19:18:21 +0300

    Update use of app components.

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,6 +1,6 @@
-import Footer from "./footer";
-import Header from "./header";
-import MainContent from "./mainContent";
+import Footer from "./Footer";
+import Header from "./Header";
+import MainContent from "./MainContent";
 
 function Page() {
     return (
